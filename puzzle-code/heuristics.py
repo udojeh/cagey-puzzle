531414d3a03b1d87c7bc0fb8995e5a2d3c307006
Hunter Coker <hunter.coker.2016@gmail.com>
2024-02-02 23:41:23 -0500

    heuristics implemented

--- puzzle-code/heuristics.py
+++ puzzle-code/heuristics.py
@@ -29,10 +29,30 @@ var_ordering == a function with the following template
 
 def ord_dh(csp):
     ''' return variables according to the Degree Heuristic '''
-    # IMPLEMENT
-    pass
+
+    vars = csp.get_all_vars()
+    max = (None, -1)
+    for var in vars:
+        var_degree = 0
+        constraints = csp.get_cons_with_var(var)
+        if (not constraints) and (not var.is_assigned()):
+            var_degree += 1
+        else:
+            for c in constraints:
+                var_degree += c.get_n_unasgn()
+        if var_degree > max[1]:
+            max = (var, var_degree)
+
+    return max[0]
 
 def ord_mrv(csp):
     ''' return variable according to the Minimum Remaining Values heuristic '''
-    # IMPLEMENT
-    pass
+
+    vars = csp.get_all_vars()
+    min = (vars[0], vars[0].cur_domain_size())
+    for var in vars[1:]:
+        var_domain_size = var.cur_domain_size()
+        if var_domain_size < min[1]:
+            min = (var, var_domain_size)
+
+    return min[0]
